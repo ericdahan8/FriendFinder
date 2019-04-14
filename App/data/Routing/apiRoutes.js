209b234846d7f741578bd9f230d72d2caefe6c90
Eric Dahan <Ericdahan@MacBook-Pro-2.local>
2019-04-14 11:25:09 -0400

    Front end completion, working on back

--- App/data/Routing/apiRoutes.js
+++ App/data/Routing/apiRoutes.js
@@ -10,6 +10,23 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    //math for friends
+    var totalDifference = 100;
+    var currDifference = 0;
+    var bestMatch;
+
+    for (var i = 0; i < friends.length; i++) {
+      for (var j = 0; j < 10; j++) {
+        //this body may be off
+        currDifference += Math.abs(req.body.scores[j] - friends[i].scores[j]);
+      }
+      console.log(i + " " + currDifference);
+      if (currDifference < totalDifference) {
+        bestMatch = friends[i];
+      }
+      currDifference = 0;
+    }
+
     //his will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     // res.json({ data: currReservations });
     //gets the data into the body
